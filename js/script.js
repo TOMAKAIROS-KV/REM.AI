295fb78c54f7b5bca8bda5f8aad045e7be9c5828
Keng Vang <tomakairos@gmail.com>
2023-07-23 18:04:16 -0500

    added intro scene, music when intro action starts by button, disabled display on opC (intro scene) and enabled display on ms1.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 let profilePic = document.querySelector(".icon");
-let mainS1 = document.querySelector(".ms1");
+let opC = document.querySelector(".opC");
+let startBtn = document.querySelector("#btn1");
+let ms1 = document.querySelector(".ms1");
+let navBar = document.querySelector(".navbar");
 
 //ONLOAD PAGE STARTS HERE
 window.onload = function() {
-    mainS1.classList.add("fadeDown");
+    opC.style.display = "flex";
+    opC.classList.add("fadeDown");
 }
 
-profilePic.addEventListener("click", function() {
-    window.location.href = '/';
-});
-
-// START BUTTON ACTION BEGINS HERE
-let startBtn = document.querySelector("#btn1");
-
+//AUDIO FUNCTIONS HERE
 const startPlay = function pageAudio() {
     var audio = document.getElementById("pageAudio");
     audio.play();
@@ -23,4 +21,25 @@ const stopPlay = function stopAudio() {
     audio.pause();
 }
 
-startBtn.addEventListener("click", startPlay);
+// START BTN ACTION HERE
+const introAnimation = function() {
+    opC.classList.add("fadeSideOut");
+
+    startPlay();
+
+    const fadeMainIn = function() {
+        navBar.style.display = "flex"; 
+        ms1.classList.add("fadeDown");
+        navBar.classList.add("fadeDown");
+    }
+
+    setTimeout(fadeMainIn, 750);
+
+    const rmOPC = function() {
+        opC.classList.remove("fadeDown");
+        opC.style.display = "none";
+    }
+    setTimeout(rmOPC, 1000);
+};
+
+startBtn.addEventListener("click", introAnimation);
